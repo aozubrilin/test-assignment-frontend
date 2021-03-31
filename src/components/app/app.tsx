3d1e83aba7c7f67e4029ead59d14e68be002a840
aozubrilin <alex14_09@mail.ru>
2021-03-31 08:57:35 +0500

    test

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,13 @@
-import * as React from "react";
-import { Redirect, Switch, Route, BrowserRouter } from "react-router-dom";
-
-import MainPage from "../../pages/main-page";
+import * as React from 'react';
+import { Redirect, Switch, Route, BrowserRouter } from 'react-router-dom';
+import MainPage from '../../pages/main-page';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path={"/"} component={MainPage} />
-        <Redirect to={"/"} />
+        <Route exact path={'/'} component={MainPage} />
+        <Redirect to={'/'} />
       </Switch>
     </BrowserRouter>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import thunk from "redux-thunk";
-import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { Provider } from "react-redux";
-import reducer from "./store/reducer/reducer";
-import "./index.scss";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import reducer from './store/reducer/reducer';
+import './index.scss';
 
-import App from "./components/app/app";
+import App from './components/app/app';
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
@@ -15,5 +15,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
